b68521687fe4e14ca2c6bc8becf935f6f9694e8b
Jiří Vrba <jiri@vrba.dev>
2023-05-12 23:03:46 +0200

    Rewrite discord bot service to provide a command line bean rather than implementing it directly

--- src/main/kotlin/dev/vrba/dubs/bot/discord/DiscordBotService.kt
+++ src/main/kotlin/dev/vrba/dubs/bot/discord/DiscordBotService.kt
@@ -7,23 +7,25 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
 
 @Service
 class DiscordBotService(
     private val configuration: DiscordConfiguration,
     private val modules: List<DiscordBotModule>
-) : CommandLineRunner {
+) {
+    @Bean
+    fun startClient(): CommandLineRunner {
+        return CommandLineRunner {
+            runBlocking {
+                val client = Kord(configuration.token)
 
-    override fun run(vararg args: String?) {
-        runBlocking {
-            val client = Kord(configuration.token)
+                client.login()
+                client.editPresence { watching("for dubs") }
 
-            client.login()
-            client.editPresence { watching("for dubs") }
-
-            modules.map { module -> async { module.register(client) } }.awaitAll()
+                modules.map { module -> async { module.register(client) } }.awaitAll()
+            }
         }
     }
-
 }
