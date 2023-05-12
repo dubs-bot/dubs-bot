816ae84e56569058e1c3d8194242821edd6a8bae
Jiří Vrba <jiri@vrba.dev>
2023-05-12 21:05:27 +0200

    Add domain class mapping for the user_scores table

---
+++ src/main/kotlin/dev/vrba/dubs/bot/domain/UserScore.kt
@@ -0,0 +1,21 @@
+package dev.vrba.dubs.bot.domain
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+import org.springframework.data.relational.core.mapping.Table
+import java.math.BigInteger
+
+@Table("user_scores")
+data class UserScore(
+    @Id
+    val id: Int = 0,
+
+    @Column("user_id")
+    val user: BigInteger,
+
+    @Column("guild_id")
+    val guild: BigInteger,
+
+    @Column("points")
+    val points: BigInteger
+)
