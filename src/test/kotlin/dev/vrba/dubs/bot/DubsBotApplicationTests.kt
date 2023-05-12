b190c9117e9df8201cb9156cfd54a459252c8c33
Jiří Vrba <jiri@vrba.dev>
2023-05-12 18:22:59 +0200

    Initial commit?

---
+++ src/test/kotlin/dev/vrba/dubs/bot/DubsBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.vrba.dubs.bot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DubsBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
