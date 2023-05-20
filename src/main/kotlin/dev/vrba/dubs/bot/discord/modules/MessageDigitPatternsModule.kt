4602536f8deaca5eb83cee4d2016eebc8e30198e
Jiří Vrba <jiri@vrba.dev>
2023-05-20 08:17:52 +0200

    Reply with embeds to rare digit pattern matches

--- src/main/kotlin/dev/vrba/dubs/bot/discord/modules/MessageDigitPatternsModule.kt
+++ src/main/kotlin/dev/vrba/dubs/bot/discord/modules/MessageDigitPatternsModule.kt
@@ -1,17 +1,20 @@
 package dev.vrba.dubs.bot.discord.modules
 
+import dev.kord.common.Color
 import dev.kord.core.Kord
+import dev.kord.core.behavior.reply
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
+import dev.kord.rest.builder.message.create.embed
 import dev.kord.x.emoji.DiscordEmoji
 import dev.kord.x.emoji.Emojis
 import dev.kord.x.emoji.addReaction
 import dev.vrba.dubs.bot.discord.DiscordBotModule
 import dev.vrba.dubs.bot.discord.toBigInteger
+import dev.vrba.dubs.bot.domain.DigitPattern
 import dev.vrba.dubs.bot.patterns.DigitsPatternRegistrar
 import dev.vrba.dubs.bot.service.UserScoreService
 import org.springframework.stereotype.Component
-import java.math.BigInteger
 
 @Component
 class MessageDigitPatternsModule(
@@ -39,6 +42,16 @@ class MessageDigitPatternsModule(
                     message.addReaction(it)
                 }
 
+            if (matches.any { it.isRare() }) {
+                message.reply {
+                    embed {
+                        color = Color(0x5865F2)
+                        title = "Whoa! Those are some pog digits!"
+                        description = "`$id`"
+                        image = "https://i.imgur.com/a31ZwOV.gif"
+                    }
+                }
+            }
 
             // Update user score in the database
             scoreService.updateScore(author, guild, matches)
@@ -47,5 +60,9 @@ class MessageDigitPatternsModule(
             scoreService.updateUser(author)
             scoreService.updateGuild(guild)
         }
+    }
+
+    private fun DigitPattern.isRare(): Boolean {
+        return this.points >= 1000
     }
 }
