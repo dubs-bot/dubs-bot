c750e48ec55fe234c9976a01e4392cfb4e8b31b6
Jiří Vrba <jiri@vrba.dev>
2023-05-24 10:11:56 +0200

    Remove the /guilds/user/{user} endpoint

--- src/main/kotlin/dev/vrba/dubs/bot/controller/GuildController.kt
+++ src/main/kotlin/dev/vrba/dubs/bot/controller/GuildController.kt
@@ -5,10 +5,8 @@ import dev.vrba.dubs.bot.response.GuildResponse
 import dev.vrba.dubs.bot.service.GuildService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import java.math.BigInteger
 
 @RestController
 @RequestMapping("/api/v1/guilds")
@@ -17,14 +15,6 @@ class GuildController(private val service: GuildService) {
     @GetMapping
     suspend fun all(): ResponseEntity<GuildResponse> {
         val guilds = service.getAllGuilds()
-        val response = GuildResponse(guilds.map { it.toDto() })
-
-        return ResponseEntity.ok(response)
-    }
-
-    @GetMapping("/user/{user}")
-    suspend fun user(@PathVariable("user") user: BigInteger): ResponseEntity<GuildResponse> {
-        val guilds = service.getGuildsWithUserScore(user)
         val response = GuildResponse(guilds.map { it.toDto() })
 
         return ResponseEntity.ok(response)
--- src/main/kotlin/dev/vrba/dubs/bot/service/GuildService.kt
+++ src/main/kotlin/dev/vrba/dubs/bot/service/GuildService.kt
@@ -2,26 +2,13 @@ package dev.vrba.dubs.bot.service
 
 import dev.vrba.dubs.bot.domain.Guild
 import dev.vrba.dubs.bot.repository.GuildRepository
-import dev.vrba.dubs.bot.repository.UserScoreRepository
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.flow.toSet
 import org.springframework.stereotype.Service
-import java.math.BigInteger
 
 @Service
 class GuildService(
-    private val guildRepository: GuildRepository,
-    private val scoreRepository: UserScoreRepository
+    private val guildRepository: GuildRepository
 ) {
-
-    suspend fun getGuildsWithUserScore(user: BigInteger): List<Guild> {
-        val ids = scoreRepository.findDistinctByUser(user).map { it.guild }.toSet()
-        val guilds = guildRepository.findByGuildIn(ids)
-
-        return guilds.toList()
-    }
-
     suspend fun getAllGuilds(): List<Guild> {
         return guildRepository.findAll().toList()
     }
