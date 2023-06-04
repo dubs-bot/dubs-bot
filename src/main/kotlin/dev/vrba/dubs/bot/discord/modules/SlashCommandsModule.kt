f298fd56b3f99a46332450beed879c2efffe7932
Jiří Vrba <jiri@vrba.dev>
2023-06-04 15:52:02 +0200

    Work in progress slash commands module

---
+++ src/main/kotlin/dev/vrba/dubs/bot/discord/modules/SlashCommandsModule.kt
@@ -0,0 +1,71 @@
+package dev.vrba.dubs.bot.discord.modules
+
+import dev.kord.common.Color
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.behavior.interaction.respondPublic
+import dev.kord.core.event.interaction.ChatInputCommandInteractionCreateEvent
+import dev.kord.core.event.interaction.GlobalChatInputCommandInteractionCreateEvent
+import dev.kord.core.on
+import dev.kord.rest.builder.message.create.InteractionResponseCreateBuilder
+import dev.kord.rest.builder.message.create.actionRow
+import dev.kord.rest.builder.message.create.embed
+import dev.vrba.dubs.bot.discord.DiscordBotModule
+import org.springframework.stereotype.Component
+
+@Component
+class SlashCommandsModule : DiscordBotModule {
+
+    override suspend fun register(client: Kord) {
+        client.createGlobalChatInputCommand("leaderboard", "Returns a link with the leaderboard for this guild")
+        client.createGlobalChatInputCommand("patterns", "Returns a link to the listing of all recognized patterns")
+
+        client.on<ChatInputCommandInteractionCreateEvent> {
+            println(this)
+            interaction.respondPublic {
+                when (interaction.invokedCommandName) {
+                    "leaderboard" -> leaderboardCommandOutput(interaction.invokedCommandGuildId)
+                    "patterns" -> patternsCommandOutput()
+                    else -> embed {
+                        title = "Unknown command"
+                        description = "Something has gone terribly wrong"
+                    }
+                }
+            }
+        }
+    }
+
+    private fun InteractionResponseCreateBuilder.leaderboardCommandOutput(guild: Snowflake?) {
+        val link = "https://dubsbot.online/leaderboard/${guild?.toString().orEmpty()}"
+
+        embed {
+            url = link
+            color = Color(0x3b88c3)
+            title = "Leaderboard for this guild"
+            thumbnail { url = "https://avatars.githubusercontent.com/u/133408315" }
+        }
+
+        actionRow {
+            linkButton(link) {
+                label = "Leaderboard"
+            }
+        }
+    }
+
+    private fun InteractionResponseCreateBuilder.patternsCommandOutput() {
+        val link = "https://dubsbot.online/patterns"
+
+        embed {
+            url = link
+            color = Color(0x3b88c3)
+            title = "The listing of all recognized digit patterns"
+            thumbnail { url = "https://avatars.githubusercontent.com/u/133408315" }
+        }
+
+        actionRow {
+            linkButton(link) {
+                label = "All recognized digit patterns"
+            }
+        }
+    }
+}
