a69f5527ac0e35a969c06bdd617b855523307124
Jiří Vrba <jiri@vrba.dev>
2023-06-14 14:16:41 +0200

    Revert "Configure correct gateway intents"
    This reverts commit b7f36c314d065bd9ec2ff64e6f158be2bf986ced.

--- src/main/kotlin/dev/vrba/dubs/bot/discord/DiscordBotService.kt
+++ src/main/kotlin/dev/vrba/dubs/bot/discord/DiscordBotService.kt
@@ -2,7 +2,6 @@ package dev.vrba.dubs.bot.discord
 
 import dev.vrba.dubs.bot.configuration.DiscordConfiguration
 import discord4j.core.DiscordClient
-import discord4j.gateway.intent.IntentSet
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
@@ -14,15 +13,12 @@ class DiscordBotService(
     @Bean
     fun startClient(): CommandLineRunner {
         return CommandLineRunner {
-            DiscordClient.create(configuration.token)
-                .gateway()
-                .setEnabledIntents(IntentSet.nonPrivileged())
-                .login()
-                .flatMap { gateway ->
-                    modules.map { it.register(gateway) }
-                        .reduce { composition, registration -> composition.and(registration) }
-                }
-                .block()
+            val login = DiscordClient.create(configuration.token).withGateway { gateway ->
+                modules.map { it.register(gateway) }
+                    .reduce { composition, registration -> composition.and(registration) }
+            }
+
+            login.block()
         }
     }
 }
